217042d0dce95aae8ccdcee2e98fa5929ecbdff3
Souvlaki42 <souvlaki420@gmail.com>
2025-06-15 15:20:03 +0300

    feat: storing toolbar_visibility in local storage

--- src/store/toolBarVisibilityStore.js
+++ src/store/toolBarVisibilityStore.js
@@ -2,9 +2,11 @@ import { create } from "zustand";
 
 export const useToolBarVisibilityStore = create(function (set) {
   return {
-    toolBarVisible: true,
+    toolBarVisible:
+      (localStorage.getItem("toolbar_visibility") ?? "true") === "true",
     setToolBarVisible: function (newVisibilityStatus) {
       set({ toolBarVisible: newVisibilityStatus });
+      localStorage.setItem("toolbar_visibility", newVisibilityStatus);
     },
   };
 });
